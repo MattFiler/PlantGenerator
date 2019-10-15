5416f0784ea98e64bf8428f1c8da0db99d36eebe
MattFiler <matthew.filer@hotmail.co.uk>
2019-10-15 17:43:14 +0100

    cam speedup on shift

--- PlantGenerator/Camera.cpp
+++ PlantGenerator/Camera.cpp
@@ -20,17 +20,21 @@ void Camera::Update(float dt)
 	GameObject::Update(dt);
 	dxshared::mView = mWorld;
 
+	double moveSpeed = dt;
+	if (InputHandler::KeyPressed(WindowsKey::SHIFT)) {
+		moveSpeed *= 4;
+	}
 	if (InputHandler::KeyPressed(WindowsKey::W)) {
-		GameObject::SetPosition(XMFLOAT3(GameObject::GetPosition().x, GameObject::GetPosition().y, GameObject::GetPosition().z - dt));
+		GameObject::SetPosition(XMFLOAT3(GameObject::GetPosition().x, GameObject::GetPosition().y, GameObject::GetPosition().z - moveSpeed));
 	}
 	if (InputHandler::KeyPressed(WindowsKey::S)) {
-		GameObject::SetPosition(XMFLOAT3(GameObject::GetPosition().x, GameObject::GetPosition().y, GameObject::GetPosition().z + dt));
+		GameObject::SetPosition(XMFLOAT3(GameObject::GetPosition().x, GameObject::GetPosition().y, GameObject::GetPosition().z + moveSpeed));
 	}
 	if (InputHandler::KeyPressed(WindowsKey::A)) {
-		GameObject::SetPosition(XMFLOAT3(GameObject::GetPosition().x + dt, GameObject::GetPosition().y, GameObject::GetPosition().z));
+		GameObject::SetPosition(XMFLOAT3(GameObject::GetPosition().x + moveSpeed, GameObject::GetPosition().y, GameObject::GetPosition().z));
 	}
 	if (InputHandler::KeyPressed(WindowsKey::D)) {
-		GameObject::SetPosition(XMFLOAT3(GameObject::GetPosition().x - dt, GameObject::GetPosition().y, GameObject::GetPosition().z));
+		GameObject::SetPosition(XMFLOAT3(GameObject::GetPosition().x - moveSpeed, GameObject::GetPosition().y, GameObject::GetPosition().z));
 	}
 	if (InputHandler::KeyPressed(WindowsKey::E)) {
 		GameObject::SetRotation(XMFLOAT3(GameObject::GetRotation().x, GameObject::GetRotation().y - dt, GameObject::GetRotation().z));
